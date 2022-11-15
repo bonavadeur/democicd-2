bf7224e07f4f0a0fd2dfdf620426868a3748cd79
bonavadeur <daodaihiep22ussr@gmail.com>
2022-11-15 15:19:23 +0700

    init

---
+++ src/main/java/com/bonavadeur/demosprboot/DemosprbootApplication.java
@@ -0,0 +1,14 @@
+package com.bonavadeur.demosprboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class DemosprbootApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(DemosprbootApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/bonavadeur/demosprboot/controller/MyController.java
@@ -0,0 +1,16 @@
+package com.bonavadeur.demosprboot.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+    @RequestMapping(value={"/hello", "hello-world"}, method= RequestMethod.GET)
+    public ResponseEntity<?> hello() {
+        User user = new User(1, "master");
+        return ResponseEntity.status(HttpStatus.OK).body(user);
+    }
+}
